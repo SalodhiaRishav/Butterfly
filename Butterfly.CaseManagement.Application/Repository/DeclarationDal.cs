2db2db86ab5d6d6ee7942679e61fdadd5d21f6b5
Haardik Sikka <haardik.sikka@nagarro.com>
2019-07-23 12:16:19 +0530

    added new folders to case management

---
+++ Butterfly.CaseManagement.Application/Repository/DeclarationDal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Butterfly.CaseManagement.Application.Repository
+{
+    public class DeclarationDal
+    {
+        int a;
+
+    }
+}
