694dce632e0f4b847f3ecbb8449ff2709dca202c
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-07-31 13:30:49 +0530

    guid

---
+++ Butterfly.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Butterfly.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Butterfly.Database.Context.ButterflyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Butterfly.Database.Context.ButterflyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
