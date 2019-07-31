7392222d3d9a59cbfbe1a969bedc71418673b653
Haardik Sikka <haardik.sikka@nagarro.com>
2019-07-31 10:29:56 +0530

    added Case context and configuration files

--- Butterfly.Database/Context/ButterflyContext.cs
+++ Butterfly.Database/Context/ButterflyContext.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Butterfly.Database.Context
 {
     using System.Data.Entity;
+
     using Butterfly.Database.Models.Declarations;
     using Butterfly.Database.Configurations.Declaration;
+    using Butterfly.Database.Models.CaseManagement;
+    using Butterfly.Database.Configurations.CaseManagement;
     public class ButterflyContext : DbContext
     {
         public ButterflyContext() : base("ButterflyContext")
@@ -18,12 +16,27 @@ namespace Butterfly.Database.Context
         public DbSet<Declaration> Declaration { get; set; }
 
         public DbSet<DropDown> DropDown { get; set; }
+        public DbSet<Case> Case { get; set; }
+        public DbSet<CaseInformation> CaseInformation {get; set;}
+
+        public DbSet<CaseReference> CaseReference { get; set; }
+
+        public DbSet<CaseStatus> CaseStatus { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Notes> Notes { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             //Configure Column
             modelBuilder.Configurations.Add(new DeclarationConfig());
             modelBuilder.Configurations.Add(new DropDownConfig());
-         
+            modelBuilder.Configurations.Add(new CaseConfigurations());
+            modelBuilder.Configurations.Add(new CaseInformationConfigurations());
+            modelBuilder.Configurations.Add(new CaseReferenceConfigurations());
+            modelBuilder.Configurations.Add(new CaseStatusConfigurations());
+            modelBuilder.Configurations.Add(new ClientConfigurations());
+            modelBuilder.Configurations.Add(new NotesConfigurations());
+;         
         }
 
     }
