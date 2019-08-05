38c6b3011b1716a0ef3655bd95e883e7550e9e1e
Haardik Sikka <hsikka95@gmail.com>
2019-08-05 15:27:47 +0530

    added handlers for dropdown and declaration

--- Butterfly.web/Declaration/AddDeclarationService.cs
+++ Butterfly.web/Declaration/AddDeclarationService.cs
@@ -5,17 +5,35 @@ using System.Web;
 namespace Butterfly.web.Declaration
 {
     using Butterfly.Declarations.Contracts.EndPoints;
-    using Butterfly.Declarations.Contracts.DeclarationDTO;
+   // using Butterfly.Declarations.Contracts.DeclarationDTO;
     using ServiceStack.ServiceInterface;
+    using Butterfly.web.CommonResponse;
+    using Butterfly.Declarations.Application.Services;
 
     public class AddDeclarationService : Service
     {
-        public bool POST(AddDeclaration NewDeclaration)
+        private readonly DeclarationBll declarationBll;
+       
+        public AddDeclarationService()
         {
-
-            var declaration = NewDeclaration.NewDeclaration;
-            // return 
-            return true;
+            declarationBll = new DeclarationBll();
+        }
+        public OperationResponse<bool> POST(AddDeclaration NewDeclaration)
+        {
+            OperationResponse<bool> response = new OperationResponse<bool>();
+            try
+            {
+                var declaration = NewDeclaration.NewDeclaration;
+                var data = declarationBll.AddDeclaration(declaration);
+                response.OnSuccess(data, "Declaration Successfully added!");
+                return response;
+            }
+            catch(Exception e)
+            {
+                response.OnException("Request to Add new declaration failed at server side.");
+                return response;
+            }
+           
         }
     }
 }
---
+++ Butterfly.web/Declaration/GetAllDeclarations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Butterfly.web.Declaration
+{
+    using Butterfly.web.CommonResponse;
+    using Butterfly.Declarations.Contracts.DeclarationDTO;
+    using Butterfly.Declarations.Application.Services;
+    public class GetAllDeclarations
+    {
+        private readonly DeclarationBll declarationBll;
+        public GetAllDeclarations()
+        {
+            declarationBll = new DeclarationBll();
+        }
+
+        public OperationResponse<IEnumerable<DeclarationDto>> GET()
+        {
+            OperationResponse<IEnumerable<DeclarationDto>> response = new OperationResponse<IEnumerable<DeclarationDto>>();
+            try
+            {
+                var data = declarationBll.GetAllDeclaration();
+                response.OnSuccess(data, "Declarations Fecthed Successfully");
+                return response;
+            }
+            catch(Exception e)
+            {
+                response.OnException("Declaration failed to fetched");
+                return response;
+            }
+        }
+    }
+}
