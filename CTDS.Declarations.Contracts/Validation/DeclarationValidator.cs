38148c517be8d9da2104b83961c2973115594d8c
Haardik Sikka <hsikka95@gmail.com>
2019-08-29 17:28:34 +0530

    updated validation comment

--- CTDS.Declarations.Contracts/Validation/DeclarationValidator.cs
+++ CTDS.Declarations.Contracts/Validation/DeclarationValidator.cs
@@ -7,12 +7,12 @@
     {
         public DeclarationValidator()
         {
-            RuleFor(x => x.ConsignorPostalCode).Matches(@"^([0-9]*)$").WithMessage("Postal Code Should a Positive Whole Number");
-            RuleFor(x => x.Amount).Matches(@"^[0-9]+(\.[0-9]+)?$").WithMessage("Amount Should a Positive Real Number");
-            RuleFor(x => x.ConsigneeCity).Matches(@"^[a-zA-z]+$").WithMessage("City Name should have characters between a-z");
-            RuleFor(x => x.ConsigneePostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code is a positive whole number");
-            RuleFor(x => x.ConsignorPostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code is a positive whole number");
-            RuleFor(x => x.DeclarantPostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code is positive whole number");
+            RuleFor(x => x.ConsignorPostalCode).Matches(@"^([0-9]*)$").WithMessage("Postal Code Should be a Positive integer");
+            RuleFor(x => x.Amount).Matches(@"^[0-9]+(\.[0-9]+)?$").WithMessage("Amount Should be a Positive Number");
+            RuleFor(x => x.ConsigneeCity).Matches(@"^[a-zA-z]+$").WithMessage("City Name should have alphabets only");
+            RuleFor(x => x.ConsigneePostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code should a positive integer");
+            RuleFor(x => x.ConsignorPostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code should a positive integer");
+            RuleFor(x => x.DeclarantPostalCode).Matches(@"^[0-9]+$").WithMessage("Postal Code should be positive integer");
            
         }
     }
