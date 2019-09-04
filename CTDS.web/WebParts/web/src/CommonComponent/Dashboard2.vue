a677bde290a36cfd8ce753e33600dff982d79eee
Haardik Sikka <hsikka95@gmail.com>
2019-09-04 11:50:13 +0530

    template for dashboard 2.0

---
+++ CTDS.web/WebParts/web/src/CommonComponent/Dashboard2.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="row">
+    <div>
+        <!-- code for topnav -->
+    </div>    
+   <div> 
+    <!-- code for side nav -->
+   </div>
+   <div>
+       <!-- code for information box and graphs !-->
+   </div>
+</div>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
