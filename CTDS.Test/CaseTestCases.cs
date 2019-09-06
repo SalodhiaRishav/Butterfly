eaa9a074930822d569cbb4953832e9588ce35d4d
Sakshi Mittal <sakshi.mittal@nagarro.com>
2019-09-06 12:41:37 +0530

    Added Test Cases for Case APIs

--- CTDS.Test/CaseTestCases.cs
+++ CTDS.Test/CaseTestCases.cs
@@ -1,6 +1,12 @@
 ﻿namespace CTDS.Test
 {
+    using CTDS.Web.CaseManagement;
+    using CTDS.CaseManagement.Contracts.Interfaces;
+
+    using Moq;
     using NUnit.Framework;
+    using CTDS.CaseManagement.Contracts.EndPoints;
+    using System.Collections.Generic;
 
     [TestFixture]
     public class CaseTestCases
@@ -9,6 +15,40 @@
         public void Should_return_message_invalidClientData_when_clientData_empty()
         {
 
+        }
+
+        [Test]
+        public void TestCaseCountAPI()
+        {
+            var caseBusinessLogic = new Mock<ICaseBusinessLogic>();
+            CaseManagementService caseManagement = new CaseManagementService(caseBusinessLogic.Object);
+
+            GetCaseCount getCaseCount = new GetCaseCount();
+
+            int count = 1;
+            caseBusinessLogic.Setup(c => c.GetCaseCount()).Returns(count);
+
+            var expectedResult = caseManagement.Get(getCaseCount);
+
+            Assert.That(expectedResult.Success, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void TestFilteredCaseCountAPI()
+        {
+            var caseBusinessLogic = new Mock<ICaseBusinessLogic>();
+            CaseManagementService caseManagement = new CaseManagementService(caseBusinessLogic.Object);
+
+            GetFilteredCaseCount getFilteredCaseCount = new GetFilteredCaseCount();
+
+            Dictionary<string, int> temp = new Dictionary<string, int>();
+            temp = null;
+
+            caseBusinessLogic.Setup(c => c.GetFilteredCaseCount()).Returns(temp);
+
+            var expectedResult = caseManagement.Get(getFilteredCaseCount);
+
+            Assert.That(expectedResult.Success, Is.EqualTo(true));
         }
     }
 }
