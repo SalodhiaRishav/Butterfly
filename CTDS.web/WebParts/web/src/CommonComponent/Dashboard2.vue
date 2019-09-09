7b2c5d48545b6781a9bcfce1e5b0ea71dd4be5a0
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-09-09 12:44:20 +0530

    added bar graph

--- CTDS.web/WebParts/web/src/CommonComponent/Dashboard2.vue
+++ CTDS.web/WebParts/web/src/CommonComponent/Dashboard2.vue
@@ -6,27 +6,15 @@
         </b-col>
     </b-row>    
         <appSideBar></appSideBar> 
-</div>
-<div>
-    <div >
-        <!-- code for topnav -->
-        <p> top nav </p>
+        <div>
         
-    </div> 
-    <div class="row">   
-            <div class="col-sm-3"> 
-    <!-- code for side nav -->
-                <p> side nav</p>
-            </div>
-            <div class="col-sm-9">
-       <!-- code for information box and graphs !-->
                  <button class="box">
                     <font-awesome-icon style="color:red; font-size:43px" icon="thumbtack"/> &nbsp; &nbsp; <span class="heading">Cases</span><br>
                     &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span class="count" >100</span>                   
                 </button>
-            </div>
-    </div>
 </div>
+</div>
+
 
 </template>
 <script>
