cfd816cd6f48c57a217134b0f121a0fe914cbb2b
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-09-17 17:03:22 +0530

    added chart in tile

---
+++ CTDS.Web/WebParts/web/src/commonComponent/Tile.vue
@@ -0,0 +1,59 @@
+<template>
+        <!-- <div
+            v-b-tooltip.hover
+            :title="tooltipTitle"
+            class="col-sm-4 col-md-2 box box-blue margin-left-39"
+          >
+            <span class="heading">{{counter}}</span>
+            <br />
+            <div>
+              <span class="counter">{{title}}</span>
+            </div>
+          </div> -->
+           <div class="col-sm-4 col-md-2 box box-blue ">
+               <div class="row">
+                   <div class="col-3">
+                    <div class="counter">{{counter}}</div>
+                    <div class="title">{{title}}</div>
+                   </div>
+                   <div class="col-9 chartDiv">
+                    <div> <line-chart :width="100" :height="100" :data="chartData" :options="chartOptions"></line-chart></div>
+                   </div>
+               </div>
+          </div>
+          
+</template>
+
+<script>
+import LineChart from "./LineChart.js";
+
+export default {
+    components:{
+        LineChart
+    },
+    props:{
+      chartOptions:{
+          type:Object,
+      },
+      chartData:{
+          type:Object,
+      },
+      tooltipTitle: {
+        type: String,
+        default: "Tooltip title"
+       },
+       counter:{
+           type:Number,
+           default:0
+       },
+       title:{
+           type:String,
+           default:"Title"
+       }
+    }
+};
+</script>
+
+<style scoped>
+@import url("./styles/tileStyle.css");
+</style>
---
+++ CTDS.Web/WebParts/web/src/commonComponent/TileChecker.vue
@@ -0,0 +1,74 @@
+<template>
+        <div class="row">
+<appTile tooltipTitle="Declarations" counter=4 title="Declarations" :chartOptions="chartOptions" :chartData="chartData"></appTile>
+<appTile tooltipTitle="Declarations" counter=4 title="Declarations" :chartOptions="chartOptions" :chartData="chartData"></appTile>
+<appTile tooltipTitle="Declarations" counter=4 title="Declarations" :chartOptions="chartOptions" :chartData="chartData"></appTile>
+<appTile tooltipTitle="Declarations" counter=4 title="Declarations" :chartOptions="chartOptions" :chartData="chartData"></appTile>
+        </div>
+</template>
+
+<script>
+import Tile from "./Tile";
+
+export default {
+   components:{
+       appTile:Tile,
+   },
+  data: () => {
+    return {
+     chartData: {
+         labels: ['M', 'Tu', 'W', 'Th', 'F', 'Sa', 'Su'],
+        datasets: [{
+            borderColor: "#0000FF",
+            pointBorderColor: "#0000FF",
+            pointBackgroundColor: "#0000FF",
+            pointHoverBackgroundColor: "#80b6f4",
+            pointHoverBorderColor: "#80b6f4",
+            pointBorderWidth: 6,
+            pointHoverRadius: 10,
+            pointHoverBorderWidth: 1,
+            pointRadius: 1,
+            fill: false,
+            borderWidth: 2,
+            data: [100, 120, 150, 170, 180, 170, 300]
+        }]
+    },
+    chartOptions: {
+         maintainAspectRatio:false,
+        legend: {
+            display: false,
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontColor: "rgba(0,0,0,0.5)",
+                    beginAtZero: true,
+                    maxTicksLimit: 3,
+                    padding: 0.5,
+                    display: false
+                },
+                gridLines: {
+                      scaleShowLabels: false,
+                    drawBorder: false,
+                     drawTicks: false,
+                    display: false
+                }
+             }],
+            xAxes: [{
+                gridLines: {
+                    drawBorder: false,
+                     drawTicks: false,
+                    display: false
+                },
+                ticks: {
+                    padding: 2,
+                    fontColor: "rgba(0,0,0,0.5)",
+                    maxTicksLimit: 7,
+                }
+            }]
+        },
+    }
+    };
+  }
+};
+</script>
